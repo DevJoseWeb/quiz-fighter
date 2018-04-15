f02fd7debb642b29116955de9265f423d7f0ed9e
Marcio <marcio@kazale.com>
2018-04-15 20:03:50 +0100

    Adicionado angular material.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {
+  BrowserAnimationsModule
+} from '@angular/platform-browser/animations';
+import { 
+  MatButtonModule
+} from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { JogoComponent } from './components';
@@ -11,7 +17,9 @@ import { AnimacaoService } from './services';
     JogoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule
   ],
   providers: [
   	AnimacaoService
